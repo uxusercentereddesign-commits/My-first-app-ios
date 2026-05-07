bede42f38d6e647c2452a4419e749d4dd3e7a4ab
Harshit Agarwal <harshit@Harshits-MacBook-Pro.local>
2026-05-07 21:50:47 +0530

    initial commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Harshit Agarwal on 06/05/26.
+//
+
+import SwiftUI
+
+struct Book {
+    let title: String
+    let author: String
+    var isRead: Bool
+    
+    var summary: String {
+        let status = isRead ? "finished" : "unread"
+        return "\(title) by \(author) (\(status))"
+    }
+}
+
+struct ContentView: View {
+    let myBook = Book(
+        title: "Design Systems",
+        author: "Alla Kholmatova",
+        isRead: true
+    )
+
+    var body: some View {
+        Text(myBook.summary)
+            .padding()
+    }
+}
+#Preview("light Mode") {
+    ContentView()
+        .preferredColorScheme(.light)
+}
+
+#Preview("Dark Mode") {
+    ContentView()
+        .preferredColorScheme(.dark)
+}
---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Harshit Agarwal on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
